6999e80575ac6908eeb2af53f9670affc44575f7
rh00018 <ryan.heideman11@gmail.com>
2026-05-28 15:25:28 +0000

    Got the Points System tracking, need to clean up UI.

--- Main.java
+++ Main.java
@@ -1,5 +1,41 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 public class Main {
+    //Variables
+    public static int points = 0;
+    public static int modifier = 1;
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        
+        //Frame, labels and such
+        JFrame frame = new JFrame("Main window");
+        
+        JButton center = new JButton(new ImageIcon("Product/Assets/Cookie1.jpg")); 
+        center.setBounds(100, 100, 500 ,500);
+        
+        JLabel tracker = new JLabel("Points: " + points);
+        tracker.setBounds(400, 50, 100, 25);
+        tracker.setFont(new Font("Arial", Font.PLAIN, 20));
+        
+        
+        
+        
+        center.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                points += modifier;
+                tracker.setText("Points: " + points);
+            }
+        });
+        
+        //adding things to window and opening window
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 600);
+        frame.add(tracker);
+        frame.add(center);
+        frame.setLayout(null);
+        frame.setVisible(true);
+        
+        
+        
     }
 }
